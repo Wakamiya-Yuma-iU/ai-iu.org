0bb685e9be3eb8c3dd05ae44c3e841b153fb5126
若宮佑真 <wakamiya626@gmail.com>
2023-09-25 11:55:16 +0900

    add-head mainpage-rm

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script context="module" lang="ts">
+  export { load };
+
+  interface ErrorProps {
+    status: number;
+    message: string;
+  }
+
+  function load({ error }: { error: ErrorProps }) {
+    return {
+      props: {
+        status: error.status,
+        message: error.message,
+      },
+    };
+  }
+</script>
